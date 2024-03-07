8f09917de43eafc66f905b1ba6042c8e3e1aef8e
Zubbek <michal.zub@outlook.com>
2024-03-07 13:18:12 +0100

    Integrate docker into the project

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres_db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
